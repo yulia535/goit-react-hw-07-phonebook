e3e193f154a3cea0e6c5a356f92d8130019e9ec4
Yulia Sukhar <yulia-s@i.ua>
2021-06-08 23:06:49 +0300

    дз6

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Phonebook from './components/Phonebook/Phonebook';
+
+const App = () => (
+  <div>
+    <Phonebook />
+  </div>
+);
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
+import contactsReducer from './reducer'
+import logger from 'redux-logger'
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const middleware = [...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }), logger];
+
+ 
+const store = configureStore({
+    reducer: {
+        contacts: contactsReducer
+    },
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware,
+})
+export default store;
